0f30a4c7573b26e24a357874a82fae713f9f057a
iejohnson <iejohnson@uiowa.edeu>
2022-05-25 14:53:46 -0500

    Finsihed through Chapter 7

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-from .models import Notes
 from django.http import Http404
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic.edit import DeleteView
+
+from .models import Notes
+from .forms import NotesForm
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
+class NotesUpdateView(UpdateView):
+    model= Notes
+    success_url = '/smart/notes'#redirect user to existing notes
+    form_class= NotesForm
+
+class NotesCreateView(CreateView):
+    model= Notes
+    success_url = '/smart/notes'#redirect user to existing notes
+    form_class= NotesForm
+
+
 
 class NotesListView(ListView):
     model = Notes
@@ -13,11 +35,3 @@ class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
 
-
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk = pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note doesn't exist")
-
-    return render(request, 'notes/notes_detail.html',{'note': note})
